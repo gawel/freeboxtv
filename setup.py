b7b40a21034169c6911eb75746669504213ab04a
gawel <devnull@localhost>
2008-10-12 19:37:34 +0000

    need zip zafe

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(name='freeboxtv',
       license='GPL',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
-      zip_safe=True,
+      zip_safe=False,
       install_requires=[
           # -*- Extra requirements: -*-
       ],
